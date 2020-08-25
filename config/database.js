6a9d25f600072c48edc91a450ff1cf7c92b55eb4
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-08-26 00:51:45 +0530

    postgres config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,21 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "ec2-52-204-20-42.compute-1.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "d3590jf9bc5v63"),
+        username: env("DATABASE_USERNAME", "xotnsbnqmwmuls"),
+        password: env(
+          "DATABASE_PASSWORD",
+          "a4edd5e5025eab66659ced4d9c8605d15bd7f8e9f754647c14b2dfc441f7127e"
+        ),
+        schema: "public",
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
